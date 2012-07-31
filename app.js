7352a75bc307bebc69b72c02904a77e9be939324
Marcin <kardigen@gmail.com>
2012-08-01 00:00:55 +0200

    heroku setup

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.get('/services.html', routes.services);
 app.get('/clients.html', routes.clients);
 app.get('/contact.html', routes.contact);
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
+  console.log("Express server listening on port %d", port);
 });
